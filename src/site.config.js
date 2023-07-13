07955cf53903e1c2d5154f0058169d6924207c42
XIJL123 <110888426+XIJL123@users.noreply.github.com>
2023-07-13 20:44:46 +0800

    Update site.config.js

--- src/site.config.js
+++ src/site.config.js
@@ -1,18 +1,11 @@
 module.exports = {
 	title: {
-		"zh-CN": "云极客工具",
-		"en-US": "Ygktool",
+		"zh-CN": "满天星河工具箱",
+		"en-US": "mtool",
 	},
 	description:
-		"云极客工具，励志做最轻盈最好用的在线工具。以工匠精神打造功能丰富的在线工具，无需下载即可免费使用",
-	keywords: ["云极客工具", "ygktool", "云极客", "云极客工具箱"],
-	author: {
-		name: "江村暮",
-		email: "contact@rene.wang",
-		twitter: "https://twitter.com/rea1DonandTrump",
-	},
-	repo: "https://github.com/rivertwilight/ygktool",
-	root: "https://www.ygktool.com",
+		"满天星河工具箱，励志做最轻盈最好用的在线工具。以工匠精神打造功能丰富的在线工具，无需下载即可免费使用",
+	keywords: ["满天星河工具箱", "mtool", "满天星河", "满天星河工具"],
 	apiConfig: {
 		url: "https://openapi.ygktool.com",
 	},
